80fa32cedb776d020ea98996b46f16532c1983e8
Andrés Rodríguez <afr@IGMacBook-Air.local>
2020-02-05 23:25:22 +0100

    Intial setup, create empty projects and add MVVMCross basic init

---
+++ XamarinTest/XamarinTest.Core/App.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace XamarinTest.Core
+{
+    public class App
+    {
+        public App()
+        {
+        }
+    }
+}
---
+++ XamarinTest/XamarinTest.Droid/MainApplication.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace XamarinTest.Droid
+{
+    public class MainApplication
+    {
+        public MainApplication()
+        {
+        }
+    }
+}
